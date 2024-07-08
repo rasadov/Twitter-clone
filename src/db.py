77fa8d1489acea082afd5dec86c97af2c42080f5
rauf <raufasadov23@gmail.com>
2024-07-08 18:31:58 +0400

    some changes

--- src/db.py
+++ src/db.py
@@ -7,7 +7,7 @@ import dotenv
 
 dotenv.load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
 Base = declarative_base()
 
